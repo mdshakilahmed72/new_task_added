ce2cdf25839623ca340144891d8501c1eba58dc1
Md Shakil Ahmed <mdshakilahmed.cse@gmail.com>
2025-06-18 00:05:49 +0600

    Updated

--- new_task/settings.py
+++ new_task/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-ukey7y5c*lg#$3x7k%y76t65v8a#oow@41fs8if!r$4%rr$3m-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://*.onrender.com','http://127.0.0.1:8000']
 
 
 # Application definition
